a99b0f94193aeb1b70c23299a43eb9dde523ac4a
Ritank Jaikar <ritank.jaikar@gmail.com>
2025-01-03 23:06:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 //your JS code here. If required.
 function allMethods() {
   //write your code here
+  console.log(Object.getOwnPropertyNames(Math));
+  return Object.getOwnPropertyNames(Math);
 }
-
 alert(allMethods());
